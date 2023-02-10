00086a91abc2148664894a0ad5352ed051fb2b70
kakachiu <gon7902191@hotmail.com>
2023-02-10 10:33:17 +0800

    0210

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -19,5 +19,23 @@ window.addEventListener("beforeinstallprompt", function (event) {
   event.preventDefault(); // 取消預設的直接跳出通知設定
   deferredPrompt = event; // 將監聽到的install banner事件傳到deferredPrompt變數
 
+  if (deferredPrompt) {
+    // 確定我們有「攔截」到chrome所發出的install banner事件
+    deferredPrompt.prompt(); // 決定要跳出通知
+
+    // 根據用戶的選擇進行不同處理，這邊我指印出log結果
+    deferredPrompt.userChoice.then(function (choiceResult) {
+      console.log(choiceResult.outcome);
+
+      if (choiceResult.outcome === "dismissed") {
+        console.log("User cancelled installation");
+      } else {
+        console.log("User added to home screen");
+        shareImageButton.style.display = "none";
+      }
+    });
+    deferredPrompt = null; // 一旦用戶允許加入後，之後就不會再出現通知
+  }
+
   return false;
 });
--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -7,22 +7,23 @@ var closeCreatePostModalButton = document.querySelector(
 function openCreatePostModal() {
   createPostArea.style.display = "block";
 
-  if (deferredPrompt) {
-    // 確定我們有「攔截」到chrome所發出的install banner事件
-    deferredPrompt.prompt(); // 決定要跳出通知
+  // if (deferredPrompt) {
+  //   // 確定我們有「攔截」到chrome所發出的install banner事件
+  //   deferredPrompt.prompt(); // 決定要跳出通知
 
-    // 根據用戶的選擇進行不同處理，這邊我指印出log結果
-    deferredPrompt.userChoice.then(function (choiceResult) {
-      console.log(choiceResult.outcome);
+  //   // 根據用戶的選擇進行不同處理，這邊我指印出log結果
+  //   deferredPrompt.userChoice.then(function (choiceResult) {
+  //     console.log(choiceResult.outcome);
 
-      if (choiceResult.outcome === "dismissed") {
-        console.log("User cancelled installation");
-      } else {
-        console.log("User added to home screen");
-      }
-    });
-    deferredPrompt = null; // 一旦用戶允許加入後，之後就不會再出現通知
-  }
+  //     if (choiceResult.outcome === "dismissed") {
+  //       console.log("User cancelled installation");
+  //     } else {
+  //       console.log("User added to home screen");
+  //       shareImageButton.style.display = "none";
+  //     }
+  //   });
+  //   deferredPrompt = null; // 一旦用戶允許加入後，之後就不會再出現通知
+  // }
 }
 
 function closeCreatePostModal() {
