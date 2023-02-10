da4d7337b9b3d71fb381fbde8439db0c8926f600
kakachiu <gon7902191@hotmail.com>
2023-02-10 10:59:25 +0800

    0210

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,22 +1,45 @@
 // 判斷瀏覽器是否支援 Service Workers
-// if ("serviceWorker" in navigator) {
-//   // 註冊 Service Workers
-//   // register 是支援 Promise
-//   navigator.serviceWorker
-//     .register("./sw.js")
-//     .then(res => {
-//       console.log("Service worker registered!");
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-// }
 if ("serviceWorker" in navigator) {
-  // register 是支援 Promise 的
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./sw.js");
-  });
+  // 註冊 Service Workers
+  // register 是支援 Promise
+  navigator.serviceWorker
+    .register("./sw.js")
+    .then(res => {
+      console.log("Service worker registered!");
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
+
+let deferredPrompt;
+const addBtn = document.querySelector(".addBtn");
+addBtn.style.display = "none";
+
+window.addEventListener("beforeinstallprompt", e => {
+  // 防止 Chrome 67 及更早版本自动显示安装提示
+  e.preventDefault();
+  // 稍后再触发此事件
+  deferredPrompt = e;
+  // 更新 UI 以提醒用户可以将 App 安装到桌面
+  addBtn.style.display = "block";
+
+  addBtn.addEventListener("click", e => {
+    // 隐藏显示 A2HS 按钮的界面
+    addBtn.style.display = "none";
+    // 显示安装提示
+    deferredPrompt.prompt();
+    // 等待用户反馈
+    deferredPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      } else {
+        console.log("User dismissed the A2HS prompt");
+      }
+      deferredPrompt = null;
+    });
+  });
+});
 
 // let deferredPrompt;
 
